4da84f24bcec77d3780ab1072aff5ad8dcd963cb
sonudaryani <sonudaryani4@gmail.com>
2023-07-22 23:10:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from "./Components/Navbar";
+import BookList from "./Components/BookList";
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 function App() {
+
+  const [books,setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+   useEffect(()=>{
+    const handleSearch = async (query) => {
+       
+       
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}`
+        );
+  
+        const searchResult = response.data.items;
+        
+        setBooks(searchResult);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    console.log(searchQuery,"show")
+    handleSearch({searchQuery})
+  },[searchQuery])
+  
+
+  useEffect(()=>{
+    
+    const DataFetch = async ()=>{
+
+      try{
+        const herryPotterBooks = await axios.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter");
+        const sharelockBooks = await axios.get("https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes"); 
+        const AllData = herryPotterBooks.data.items.concat(sharelockBooks.data.items)
+        setBooks(()=>AllData);
+      }
+      catch(error){console.log(error)}
+    }
+    DataFetch()
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <Navbar onSearch={setSearchQuery}/>
+      { books.length>0 && <BookList books={books}/> }
     </div>
   );
 }
